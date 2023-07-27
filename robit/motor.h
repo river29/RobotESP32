65e7567cfacc8940dc39ef67a5a3ffa79fdfe6ca
Tucker Shannon <tuckerleeshannon@gmail.com>
2023-07-27 12:47:51 -0700

    Update motor.h

--- robit/motor.h
+++ robit/motor.h
@@ -3,13 +3,10 @@
 
 #include <PID_v1.h>
 #include <Encoder.h>
-#include <ESP32Servo.h>
 
 #define KP 2.0
 #define KI 5.0
 #define KD 1.0
-#define MIN_PULSE_WIDTH 544
-#define MAX_PULSE_WIDTH 2400
 
 class Motor
 {
@@ -18,11 +15,13 @@ private:
   double Setpoint, Input, Output;
   PID myPID;
   Servo myESC;
+  int totalTicks;
 public:
   Motor(int encoderPinA, int encoderPinB, int escPin) : myEnc(encoderPinA, encoderPinB), myPID(&Input, &Output, &Setpoint, KP, KI, KD, DIRECT)
   {
     myESC.attach(escPin, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH);
     myPID.SetMode(AUTOMATIC);
+    totalTicks = 0; // Initialize totalTicks to 0
   }
 
   void setSpeed(int speed)
@@ -37,6 +36,13 @@ public:
     int pulseWidth = map(static_cast<int>(Output), 0, 1023, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH);
     myESC.writeMicroseconds(pulseWidth);
   }
+
+  int getTotalTicks()
+  {
+    return myEnc.read();
+  }
 };
 
+
 #endif // MOTOR_H
+
