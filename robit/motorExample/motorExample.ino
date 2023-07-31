169acf5a8767d113c959e6bde756af5992084810
Tucker Shannon <tuckerleeshannon@gmail.com>
2023-07-31 09:27:33 -0700

    Update motorExample.ino

--- robit/motorExample/motorExample.ino
+++ robit/motorExample/motorExample.ino
@@ -4,13 +4,15 @@ class Motor {
     int in2Pin;
     int enablePin;
     int channel;
+    String motorName;
 
   public:
-    Motor(int in1Pin, int in2Pin, int enablePin, int channel) {
+    Motor(int in1Pin, int in2Pin, int enablePin, int channel, String motorName) {
       this->in1Pin = in1Pin;
       this->in2Pin = in2Pin;
       this->enablePin = enablePin;
       this->channel = channel;
+      this->motorName = motorName;
       pinMode(in1Pin, OUTPUT);
       pinMode(in2Pin, OUTPUT);
       ledcSetup(channel, 5000, 8);
@@ -21,18 +23,24 @@ class Motor {
       if (direction == 0) {
         digitalWrite(in1Pin, LOW);
         digitalWrite(in2Pin, HIGH);
+        Serial.println(motorName + " running backward at " + String(speedPercentage) + "% speed");
       } else {
         digitalWrite(in1Pin, HIGH);
         digitalWrite(in2Pin, LOW);
+        Serial.println(motorName + " running forward at " + String(speedPercentage) + "% speed");
       }
       int dutyCycle = speedPercentage * 255 / 100; // convert percentage to duty cycle
       ledcWrite(channel, dutyCycle);
+
+      if(speedPercentage == 0) {
+        Serial.println(motorName + " stopped");
+      }
     }
 };
 
 // create motor objects
-Motor motorA(4, 16, 19, 0); // IN1, IN2, ENA, channel
-Motor motorB(17, 18, 21, 1); // IN3, IN4, ENB, channel
+Motor motorA(4, 16, 19, 0, "Motor A"); // IN1, IN2, ENA, channel, motor name
+Motor motorB(17, 18, 21, 1, "Motor B"); // IN3, IN4, ENB, channel, motor name
 
 void setup() {
   Serial.begin(115200);
@@ -40,35 +48,27 @@ void setup() {
 }
 
 void loop() {
-  Serial.println("Motor A running forward at 5% speed");
   motorA.setSpeed(1, 100); // direction, speed (percentage)
   delay(1000);
 
   motorA.setSpeed(1, 0); // stop motor A
-  Serial.println("Motor A stopped");
   delay(1000);
 
-  Serial.println("Motor A running backward at 5% speed");
   motorA.setSpeed(0, 100); // direction, speed (percentage)
   delay(1000);
 
   motorA.setSpeed(0, 0); // stop motor A
-  Serial.println("Motor A stopped");
   delay(1000);
 
-  Serial.println("Motor B running forward at 5% speed");
   motorB.setSpeed(1, 100); // direction, speed (percentage)
   delay(1000);
 
   motorB.setSpeed(1, 0); // stop motor B
-  Serial.println("Motor B stopped");
   delay(1000);
 
-  Serial.println("Motor B running backward at 5% speed");
   motorB.setSpeed(0, 100); // direction, speed (percentage)
   delay(1000);
 
   motorB.setSpeed(0, 0); // stop motor B
-  Serial.println("Motor B stopped");
   delay(1000);
 }
