114f9d7c56611751b9ef0403791dff140287eddf
Tucker Shannon <tuckerleeshannon@gmail.com>
2023-08-11 16:47:56 -0700

    Update robit.ino

--- robit/robit.ino
+++ robit/robit.ino
@@ -6,11 +6,13 @@
 #include <ArduinoJson.h>
 #include <ESPmDNS.h>
 
+
 #define WIFI_SSID "ssid"
 #define WIFI_PASS "pass"
 
 Motor* motorA;
 Motor* motorB;
+
 
 AsyncWebServer server(80);
 AsyncWebSocket ws("/ws");
@@ -21,6 +23,9 @@ void setup() {
 
   motorA = new Motor(4, 16, 19, 0, "Motor A", 23, 22); // IN1, IN2, ENA, channel, motor name, encoderPinA, encoderPinB
   motorB = new Motor(17, 18, 21, 1, "Motor B", 34, 39); // IN3, IN4, ENB, channel, motor name, encoderPinA, encoderPinB
+  motorA->setMaxSpeed(7);
+  motorA->setMaxSpeed(7);
+
   WiFi.begin(WIFI_SSID, WIFI_PASS);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
@@ -65,6 +70,9 @@ void loop() {
     doc["motorB"] = motorB->getTotalTicks();
     doc["motorB_rpm"] = motorB->getRPM();
     doc["motorA_speed"] = motorA->getMaxSpeed();
+    doc["KI"] = motorA->getKI();
+    doc["KP"] = motorA->getKP();
+    doc["kD"] = motorA->getKD();
     
     String msg;
     serializeJson(doc, msg);
@@ -87,27 +95,29 @@ void onWsEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventTyp
     if (doc.containsKey("changeMaxSpeed")) {
       int direction = doc["changeMaxSpeed"];
       int newSpeed = motorA->getMaxSpeed() + direction;
-      newSpeed = constrain(newSpeed, 1, 14); // Ensure the speed is within the range [1, 10]
+      newSpeed = constrain(newSpeed, 1, 10); // Ensure the speed is within the range [1, 10]
       
       motorA->setMaxSpeed(newSpeed);
       motorB->setMaxSpeed(newSpeed);
     }
 
-    if (doc.containsKey("axis2") && doc.containsKey("axis5")) {
+    if (doc.containsKey("axis1") && doc.containsKey("axis2")) {
       // Extract speed values
+      double axis1 = doc["axis1"];
       double axis2 = doc["axis2"];
-      double axis5 = doc["axis5"];
-      axis2 = axis2/(10-7*(abs(axis5)/1.0));
-      double motorBSpeed = min(100.0, max(-100.0, (axis2 - axis5) * 100.0));
-      double motorASpeed = min(100.0, max(-100.0, (-axis5 - axis2) * 100.0));
+      axis1 = axis1/(10-7*(abs(axis2)/1.0));
+      double motorBSpeed = min(100.0, max(-100.0, (axis1 - axis2) * 100.0));
+      double motorASpeed = min(100.0, max(-100.0, (-axis2 - axis1) * 100.0));
 
       // Set motor speeds
-      Serial.println("Motor A Speed: " + String(motorASpeed) + "% speed" + " Motor B Speed: " + String(motorBSpeed) + "% speed");
+      // Serial.println("Motor A Speed: " + String(motorASpeed) + "% speed" + " Motor B Speed: " + String(motorBSpeed) + "% speed");
       
       motorA->setSpeed(motorASpeed < 0, abs(motorASpeed));
       // Serial.println(String(motorBSpeed));
       motorB->setSpeed(motorBSpeed > 0, abs(motorBSpeed));
     }
+
+
 
     if (doc.containsKey("kp") && doc.containsKey("ki") && doc.containsKey("kd")) {
       // Extract PID values
@@ -129,15 +139,11 @@ return R"rawliteral(
 <html>
   <body>
     <div id="controllerInfo"></div>
-    <div id="controlPad" style="width: 80vw; height: 80vw; position: relative; border: 1px solid black;">
-      <div id="verticalLine" style="width: 1px; height: 100%; position: absolute; top: 0; left: 50%; background: black;"></div>
-      <div id="horizontalLine" style="width: 100%; height: 1px; position: absolute; top: 50%; left: 0; background: black;"></div>
-      <div id="cursor" style="width: 10px; height: 10px; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); background: red;"></div>
-    </div>
     <br>
     <div id="motorInfo">
       <div id="leftMotor">Left Motor: <span id="leftDirection">&#8593;</span> <span id="leftSpeed">0</span>% <span id="leftEncoder">0</span></div>
       <div id="rightMotor">Right Motor: <span id="rightDirection">&#8593;</span> <span id="rightSpeed">0</span>% <span id="rightEncoder">0</span></div>
+      <div id="pid">KP: <span id="KP">0</span> KI: <span id="KI">0</span> KD: <span id="KP">0</span></div>
     </div>
     <p>Adjust PID Settings:</p>
     <label>KP: <input type="text" id="kp" value="2.0"></label>
@@ -157,47 +163,59 @@ return R"rawliteral(
 
 function updateStatus(){
   var gp = navigator.getGamepads()[0];
+  var axis1Int = 0;
+  var axis2Int = 5;
+  var b1Int = 6;
+  var b2Int = 7;
   if(gp){
     var controllerInfo = document.getElementById("controllerInfo");
+    
+    if (isMobileDevice()) {
+      axis1Int = 0;
+      axis2Int = 3;
+      b1Int = 4;
+      b2Int = 5;
+    } 
 
-    var axis2 = gp.axes[0];
-    var axis5 = gp.axes[5];
-
+    // Round axis2 and axis5 to the nearest hundreth
+    var axis1 = parseFloat(gp.axes[axis1Int].toFixed(2));
+    var axis2 = parseFloat(gp.axes[axis2Int].toFixed(2));
     // Handle B6 and B7 buttons
-    var b6 = gp.buttons[6].pressed;
-    var b7 = gp.buttons[7].pressed;
+    var b1 = gp.buttons[b1Int].pressed;
+    var b2 = gp.buttons[b2Int].pressed;
 
-    if(b6) {
+    if(b1) {
       connection.send(JSON.stringify({ changeMaxSpeed: -1 }));
     }
 
-    if(b7) {
+    if(b2) {
       connection.send(JSON.stringify({ changeMaxSpeed: 1 }));
     }
 
-    controllerInfo.innerHTML = "AXIS 2: " + axis2 + ", AXIS 5: " + axis5 + "<br />";
+    controllerInfo.innerHTML = "AXIS 1: " + axis1 + ", AXIS 2: " + axis2 + "<br />";
     // controllerInfo.innerHTML += "motorASpeed: " + motorASpeed + ", motorBSpeed: " + motorBSpeed + ", dir: " + dir + ", Max Speed: " + motorA.getMaxSpeed();
 
-    connection.send(JSON.stringify({ axis2: axis2, axis5: axis5}));
+    connection.send(JSON.stringify({ axis1: axis1, axis2: axis2}));
   }
 }
-      var controlPad = document.getElementById("controlPad");
-      var cursor = document.getElementById("cursor");
+
       var leftMotor = document.getElementById("leftSpeed");
       var rightMotor = document.getElementById("rightSpeed");
       var leftDirection = document.getElementById("leftDirection");
       var rightDirection = document.getElementById("rightDirection");
       var leftEncoder = document.getElementById("leftEncoder");
       var rightEncoder = document.getElementById("rightEncoder");
+      var pid = document.getElementById("pid");
 
       connection.onmessage = function(e) {
         var data = JSON.parse(e.data);
         if (data.hasOwnProperty('motorA') && data.hasOwnProperty('motorB')) {
           leftEncoder.innerText = data.motorA + " ticks, " + data.motorA_rpm + " rpm" + " speed: " + data.motorA_speed;
           rightEncoder.innerText = data.motorB + " ticks, " + data.motorB_rpm + " rpm";
+          pid.innerText = "PID: " + " KP: " + data.KP + " KI: " + data.KI + " KD: " + data.KD;
         }
       };
-  
+
       function updatePID() {
         var kp = document.getElementById("kp").value;
         var ki = document.getElementById("ki").value;
@@ -206,6 +224,11 @@ function updateStatus(){
         // Send PID values over WebSocket connection
         connection.send(JSON.stringify({ kp: kp, ki: ki, kd: kd }));
       }
+      function isMobileDevice() {
+        return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+      }
+
+
     </script>
   </body>
 </html>
